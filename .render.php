ff87fd0d61e45579cde3941a60cc949e6dd529d6
Anthonny Mosquera <130510821+AnthonnyM31@users.noreply.github.com>
2025-11-11 16:21:52 -0500

    Delete .render.php

--- .render.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Este archivo asegura que la configuración de PHP se cargue correctamente
-// en el entorno de Render antes de que se ejecuten los comandos.
-// No se requiere código específico de Laravel aquí.
